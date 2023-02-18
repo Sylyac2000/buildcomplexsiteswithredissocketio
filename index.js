b60ca72e45055ab830f22b8a1b228f0ffaa91712
sylyac2000 <sylyac2000@gmail.com>
2023-02-18 07:43:02 +0000

    vid4.4 broadcast to other than me

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ const io = socketio(server)
 //load a middleware
 app.use(express.static('static'))
 
-io.on('connection', function (socket) {
-    console.log('a socket is opened');
-    //console.log(socket);
+/*The socket object is a connection from one browser to the server,
+   while the IO object is a server object, which will know about all
+   the other connections*/
+io.on('connection',  (socket) => {
+    //send to all without me
+    socket.broadcast.emit('user.events', 'someone has joined')
 
-    socket.on('socketping', () => {
-        console.log('Received socketping, sending socketpong')
-        socket.emit('socketpong');
+    socket.on('name', (name) => {
+        console.log(name + ' says Hello!');
+        //send to all without me
+        socket.broadcast.emit('name', name);
     })
 });
